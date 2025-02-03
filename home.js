13df6bbfeeb94175ca0a068c9857139d4f20a6ad
Baskar-krabv <baskarkrab0@gmail.com>
2025-02-03 22:06:26 +0530

    Updated

--- home.js
+++ home.js
@@ -9,15 +9,31 @@ myAi.addEventListener("click",()=>{
     document.querySelector(".ai").style.display="block";
     document.querySelector(".recents").style.display="none";
     document.querySelector('.profile').style.display="none";
+    document.querySelector('.home').style.display="none";
 })
 recentes.addEventListener("click",()=>{
     document.querySelector(".recents").style.display="block";
     document.querySelector(".ai").style.display="none";
     document.querySelector('.profile').style.display="none";
+    document.querySelector('.home').style.display="none";
 })
 profile.addEventListener("click",()=>{
     document.querySelector(".recents").style.display="none";
     document.querySelector(".ai").style.display="none";
     document.querySelector('.profile').style.display="block";
     document.querySelector('.profile').style.display="flex";
+    document.querySelector('.home').style.display="none";
+})
+home.addEventListener("click",()=>{
+    document.querySelector('.home').style.display="block";
+    document.querySelector(".recents").style.display="none";
+    document.querySelector(".ai").style.display="none";
+    document.querySelector('.profile').style.display="none";
+})
+document.getElementById('src').addEventListener("click",()=>{
+    var userMsg=document.getElementById('userInpt').value;
+    var umsg=document.createElement('h4');
+    umsg.classList.add('userMsg');
+    umsg.textContent=userMsg;
+    document.querySelector('.codeGeneratingArea').appendChild(umsg);
 })
